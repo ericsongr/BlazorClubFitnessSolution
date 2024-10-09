9eabf58c194fc4d5bf6c55e5a0a031196d14ea56
Ericson Ramos <ericsongr@yahoo.com>
2024-10-09 21:32:14 +0800

    polish the validation for account and category #8

---
+++ ClubFitnessDomain/Validators/AccountSupplierValidator.cs
@@ -0,0 +1,29 @@
+﻿using FluentValidation;
+using ClubFitnessDomain.Dtos;
+
+namespace ClubFitnessDomain.Validators
+{
+    public class AccountSupplierValidator : AbstractValidator<AccountSupplier>
+    {
+        public AccountSupplierValidator()
+        {
+            RuleFor(account => account.AccountId)
+                .NotEmpty().WithMessage("Please select account.");
+
+            RuleFor(account => account.SupplierName)
+                .NotEmpty().WithMessage("Supplier name is required.");
+
+            RuleFor(account => account.ContactFirstName)
+                .NotEmpty().WithMessage("First name is required.");
+
+            RuleFor(account => account.ContactLastName)
+                .NotEmpty().WithMessage("Last name is required.");
+
+            RuleFor(account => account.ContactEmail)
+                .NotEmpty().WithMessage("Email is required.");
+
+            RuleFor(account => account.ContactMobile)
+                .NotEmpty().WithMessage("Mobile number is required.");
+        }
+    }
+}
