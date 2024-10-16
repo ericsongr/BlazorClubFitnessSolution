2c850c3d1fc045dc8abc6a891164a03a0b3c7406
Ericson Ramos <ericsongr@yahoo.com>
2024-10-16 09:18:09 +0800

    group notification #37

---
+++ ClubFitnessSolution/Models/GroupNotification.cs
@@ -0,0 +1,8 @@
+﻿namespace ClubFitnessSolution.Models
+{
+    public class GroupNotification
+    {
+        public IEnumerable<string> Roles { get; set; } = new List<string>
+            { "All", "Supervisor", "Administrator", "Manager", "Employee" };
+    }
+}
--- ClubFitnessSolution/Services/Hubs/ChatHub.cs
+++ ClubFitnessSolution/Services/Hubs/ChatHub.cs
@@ -5,7 +5,7 @@ using Microsoft.AspNetCore.SignalR;
 
 namespace ClubFitnessSolution.Services.Hubs
 {
-    public class ChatHub : Hub<IChatClient>
+    public class ChatHub : Hub<IHub>
     {
         public override Task OnConnectedAsync()
         {
@@ -15,6 +15,21 @@ namespace ClubFitnessSolution.Services.Hubs
         public async Task SendMessage(ChatMessage request)
         {
             await Clients.All.ReceiveMessage(request);
+        }
+
+        public async Task SendGroupMessage(UserGroup userGroup)
+        {
+            await Clients.Group(userGroup.SelectedRole).ReceiveGroupMessage(userGroup);
+        }
+
+        public async Task ConnectToGroup(string role)
+        {
+            await Groups.AddToGroupAsync(Context.ConnectionId, role);
+        }
+
+        public async Task DisconnectToGroup(string role)
+        {
+            await Groups.AddToGroupAsync(Context.ConnectionId, role);
         }
     }
 }
