0220a8aa76e4b899acea9a697a1f4c854e928ea0
Ericson Ramos <ericsongr@yahoo.com>
2024-10-16 22:41:32 +0800

    add spinner and make base razor page to use across pages #40

---
+++ ClubFitnessSolution/Components/BasePage.razor.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Components;
+
+namespace ClubFitnessSolution.Components
+{
+    public class BasePage : ComponentBase
+    {
+        protected bool IsLoading { get; private set; } = true; // Default to true
+
+        protected override async Task OnInitializedAsync()
+        {
+            IsLoading = true; // Start loading state
+            await LoadDataAsync();
+            IsLoading = false; // End loading state
+        }
+
+        // Derived classes will override this method to load specific data
+        protected virtual Task LoadDataAsync()
+        {
+            return Task.CompletedTask;
+        }
+    }
+}
